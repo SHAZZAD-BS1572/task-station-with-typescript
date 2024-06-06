1109f01c160ab64c077818c9c8db1c419618ca6f
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-06-06 18:47:00 +0600

    updated

--- Tests/editingTest.spec.ts
+++ Tests/editingTest.spec.ts
@@ -1,10 +1,13 @@
 import { expect, test } from "../base/basePage";
 import * as data from "../testData/loginData.json";
-
+test.beforeEach("Task Edit", async ({ page, baseURL, loginPage,editingPage }) => {
+  await page.goto(`${baseURL}auth/login`);
+  await loginPage.login(data.email, data.password);
+  });
 test.describe("Task Station automation", async () => {  
     test("Task Edit", async ({ page, baseURL, loginPage,editingPage }) => {
-      await page.goto(`${baseURL}auth/login`);
-      await loginPage.login(data.email, data.password);
+      // await page.goto(`${baseURL}auth/login`);
+      // await loginPage.login(data.email, data.password);
       await editingPage.clickDetail(data.taskTitle)
       await editingPage.editDetail(data.project,data.tag,data.description,data.oldTime,data.date,data.editiedTime,data.remark)
 
@@ -12,22 +15,22 @@ test.describe("Task Station automation", async () => {
     });
 
     test("Todo to Complete ", async ({ page, baseURL, loginPage,editingPage }) => {
-      await page.goto(`${baseURL}auth/login`);
-      await loginPage.login(data.email, data.password);
+      // await page.goto(`${baseURL}auth/login`);
+      // await loginPage.login(data.email, data.password);
       await editingPage.clickDetail(data.taskTitle)
       await editingPage.todoToComplete()
     });
 
     test("Complete To Blocker ", async ({ page, baseURL, loginPage,editingPage }) => {
-      await page.goto(`${baseURL}auth/login`);
-      await loginPage.login(data.email, data.password);
+      // await page.goto(`${baseURL}auth/login`);
+      // await loginPage.login(data.email, data.password);
       await editingPage.clickDetail(data.taskTitle)
       await editingPage.completeToBlocker()
     });
 
     test("Blocker To Todo", async ({ page, baseURL, loginPage,editingPage }) => {
-      await page.goto(`${baseURL}auth/login`);
-      await loginPage.login(data.email, data.password);
+      // await page.goto(`${baseURL}auth/login`);
+      // await loginPage.login(data.email, data.password);
       await editingPage.clickDetail(data.taskTitle)
       await editingPage.blockerToTodo()
     });
